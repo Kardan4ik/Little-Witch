6ab1f747ec9dcf2685e838df75773736113394a3
Kardan4ik <Ka10Dan@yandex.ru>
2026-03-17 21:49:38 +0300

    img

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed = 1000.0
+@export var speed = 200.0
 
 func _physics_process(delta):
 	var direction = Vector2.ZERO
--- enemy.gd
+++ enemy.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
-@export var speed = 1.0
+@export var speed = 150.0
 var player = null
 
 func _ready():
 	# Ищем игрока в группе "player"
 	player = get_tree().get_first_node_in_group("player")
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if player:
 		# Поворачиваемся к игроку
 		look_at(player.position)
